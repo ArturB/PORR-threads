c06aab1a013cb5bceab5e5055f892394522f18b3
Artur Brodzki <artur.brodzki@gmail.com>
2018-10-29 11:01:10 +0100

    include stack-work for faster build

---
+++ Multilinear/.stack-work/dist/5c8418a7/build/autogen/Paths_Multilinear.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE CPP #-}
+{-# OPTIONS_GHC -fno-warn-missing-import-lists #-}
+{-# OPTIONS_GHC -fno-warn-implicit-prelude #-}
+module Paths_Multilinear (
+    version,
+    getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir,
+    getDataFileName, getSysconfDir
+  ) where
+
+import qualified Control.Exception as Exception
+import Data.Version (Version(..))
+import System.Environment (getEnv)
+import Prelude
+
+#if defined(VERSION_base)
+
+#if MIN_VERSION_base(4,0,0)
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#else
+catchIO :: IO a -> (Exception.Exception -> IO a) -> IO a
+#endif
+
+#else
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#endif
+catchIO = Exception.catch
+
+version :: Version
+version = Version [0,8,0,0] []
+bindir, libdir, dynlibdir, datadir, libexecdir, sysconfdir :: FilePath
+
+bindir     = "D:\\Projekty\\Studia\\PORR-threads\\.stack-work\\install\\c1f464f3\\bin"
+libdir     = "D:\\Projekty\\Studia\\PORR-threads\\.stack-work\\install\\c1f464f3\\lib\\x86_64-windows-ghc-8.2.2\\Multilinear-0.8.0.0-4dpV53DlxV5EwwV6FZreVP"
+dynlibdir  = "D:\\Projekty\\Studia\\PORR-threads\\.stack-work\\install\\c1f464f3\\lib\\x86_64-windows-ghc-8.2.2"
+datadir    = "D:\\Projekty\\Studia\\PORR-threads\\.stack-work\\install\\c1f464f3\\share\\x86_64-windows-ghc-8.2.2\\Multilinear-0.8.0.0"
+libexecdir = "D:\\Projekty\\Studia\\PORR-threads\\.stack-work\\install\\c1f464f3\\libexec\\x86_64-windows-ghc-8.2.2\\Multilinear-0.8.0.0"
+sysconfdir = "D:\\Projekty\\Studia\\PORR-threads\\.stack-work\\install\\c1f464f3\\etc"
+
+getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
+getBinDir = catchIO (getEnv "Multilinear_bindir") (\_ -> return bindir)
+getLibDir = catchIO (getEnv "Multilinear_libdir") (\_ -> return libdir)
+getDynLibDir = catchIO (getEnv "Multilinear_dynlibdir") (\_ -> return dynlibdir)
+getDataDir = catchIO (getEnv "Multilinear_datadir") (\_ -> return datadir)
+getLibexecDir = catchIO (getEnv "Multilinear_libexecdir") (\_ -> return libexecdir)
+getSysconfDir = catchIO (getEnv "Multilinear_sysconfdir") (\_ -> return sysconfdir)
+
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName name = do
+  dir <- getDataDir
+  return (dir ++ "\\" ++ name)
