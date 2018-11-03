01c3fc99b4f36d2fe890debd3c70a4495fe2f367
Artur Brodzki <artur.brodzki@gmail.com>
2018-11-03 01:56:42 +0100

    sample io

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,25 @@
+module Main (
+    main
+) where
+
+import           Control.Exception.Base
+import           Control.Monad.Trans.Except
+import           Control.Monad.Trans.Class
+import           Multilinear.Class
+import           Multilinear.Generic
+import           Multilinear.Generic.Serialize
+
+getCSV :: ExceptT String IO ()
+getCSV = do
+    trainImages :: Tensor Double <- fromCSVFile "mnist/train-images.csv" ',' "ij"
+    t10kImages  :: Tensor Double <- fromCSVFile "mnist/t10k-images.csv" ',' "ij"
+    lift $ print "\n"
+    lift $ putStrLn $ "train-images indices: " ++ show (indices trainImages)
+    lift $ putStrLn $ "t10k-images indices: "  ++ show (indices t10kImages)
+
+-- ENTRY POINT
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  runExceptT getCSV
+  return ()
+  
