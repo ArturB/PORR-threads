b6219ffdcf07e00815a9748d0224b970f243cf10
BZWGolem <kapoplawska@gmail.com>
2018-11-12 13:34:23 +0100

    read labels

--- test/Spec.hs
+++ test/Spec.hs
@@ -12,7 +12,11 @@ import           Multilinear.Generic.Serialize
 getCSV :: ExceptT String IO ()
 getCSV = do
     trainImages :: Tensor Double <- fromCSVFile "mnist/train-images.csv" ',' "ij"
+    labelImages  :: Tensor Double <- fromCSVFile "mnist/train-labels.csv" ',' "ij"
+
     t10kImages  :: Tensor Double <- fromCSVFile "mnist/t10k-images.csv" ',' "ij"
+    l10kImages  :: Tensor Double <- fromCSVFile "mnist/t10k-labels.csv" ',' "ij"
+    
     lift $ print "\n"
     lift $ putStrLn $ "train-images indices: " ++ show (indices trainImages)
     lift $ putStrLn $ "t10k-images indices: "  ++ show (indices t10kImages)
