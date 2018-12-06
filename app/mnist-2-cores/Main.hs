1f485d58931f00ac89eacd2cf0d91fcc429bf905
Artur Brodzki <artur@brodzki.org>
2018-12-06 13:49:32 +0100

    Untested build 18340.46143

--- app/mnist-2-cores/Main.hs
+++ app/mnist-2-cores/Main.hs
@@ -4,10 +4,6 @@ module Main (
 
 import           PORR2.Mnist.MultiCore
 
--- Number of iterations the perceptron is learned
-learnIters :: Int
-learnIters = 20
-
 -- | ENTRY POINT
 main :: IO ()
-main = testAccuracy learnIters
+main = testAccuracy 20
